5fed0b53901cdb49ded2b31cb4ead82d124557f3
Sergiusz01 <serhii.bondarczuk@icloud.com>
2025-01-29 02:00:51 +0100

    fix: popraw obsługę wylogowania

--- src/components/sharing/Logout.tsx
+++ src/components/sharing/Logout.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import { SignOutButton } from "@clerk/nextjs";
+import { SignOutButton, useClerk } from "@clerk/nextjs";
 
 const Logout = ({ children }: { children: React.ReactNode }) => {
+	const { signOut } = useClerk();
+
+	const handleSignOut = async () => {
+		await signOut();
+		window.location.href = "/";
+	};
+
 	return (
-		<SignOutButton
-			signOutCallback={() => {
-				window.location.href = "/";
-			}}
-		>
+		<div onClick={handleSignOut}>
 			{children}
-		</SignOutButton>
+		</div>
 	);
 };
 
